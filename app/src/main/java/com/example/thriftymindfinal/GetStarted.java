43a98a7b7859d2970c41ca359479fb9499ad6f58
ritchelmendaros <ritchel.mendaros@cit.edu>
2023-05-15 22:38:10 +0800

    Modified GetStarted

--- app/src/main/java/com/example/thriftymindfinal/GetStarted.java
+++ app/src/main/java/com/example/thriftymindfinal/GetStarted.java
@@ -2,13 +2,24 @@ package com.example.thriftymindfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GetStarted extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
+        button = (Button) findViewById(R.id.btnGetStarted);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(GetStarted.this, Dashboard.class);
+                startActivity(intent);
+            }
+        });
     }
 }
