c59b512d9319234509b64d6aca81d7b257443e9c
ritchelmendaros <ritchel.mendaros@cit.edu>
2023-05-19 21:52:12 +0800

    Added Tip of the Day

--- app/src/main/java/com/example/thriftymindfinal/Dashboard.java
+++ app/src/main/java/com/example/thriftymindfinal/Dashboard.java
@@ -1,10 +1,15 @@
 package com.example.thriftymindfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -12,13 +17,14 @@ import java.util.List;
 public class Dashboard extends AppCompatActivity {
     String[] goal_data = {"Food", "Allowance", "Projects"};
     int count = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
         List<String> items = new LinkedList<>();
-        items.add("Tansportation");
+        items.add("Transportation");
 
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -31,5 +37,28 @@ public class Dashboard extends AppCompatActivity {
             goal.notifyItemInserted(0);
             recyclerView.scrollToPosition(0);
         });
+
+        // Show the tip/notification dialog
+        showTipDialog();
     }
+
+    private void showTipDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AlertDialogTheme);
+        builder.setTitle("TIP OF THE DAY")
+                .setMessage("Cut down on unnecessary expenses by making your own coffee at home " +
+                        "instead of buying it from cafes. Investing in a good quality coffee maker " +
+                        "can save you a significant amount of money in the long run.")
+                .setPositiveButton("Thanks!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Positive button click logic
+                        dialog.dismiss();
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
 }
+
