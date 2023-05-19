a2a86e56bdd8b1cfcd9586bfd774747a274d228f
ritchelmendaros <ritchel.mendaros@cit.edu>
2023-05-19 20:41:59 +0800

    Added RecyclerView in Dashboard

--- app/src/main/java/com/example/thriftymindfinal/Dashboard.java
+++ app/src/main/java/com/example/thriftymindfinal/Dashboard.java
@@ -1,42 +1,35 @@
 package com.example.thriftymindfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.TextView;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class Dashboard extends AppCompatActivity {
-
+    String[] goal_data = {"Food", "Allowance", "Projects"};
+    int count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-//        TableLayout tableLayout = findViewById(R.id.tbldashboard);
-//
-//// Create table rows
-//        for (int i = 0; i < 3; i++) {
-//            TableRow tableRow = new TableRow(this);
-//            tableRow.setLayoutParams(new TableLayout.LayoutParams(
-//                    TableLayout.LayoutParams.MATCH_PARENT,
-//                    TableLayout.LayoutParams.WRAP_CONTENT));
-//
-//            // Create text views for each cell in the row
-//            for (int j = 0; j < 3; j++) {
-//                TextView textView = new TextView(this);
-//                textView.setLayoutParams(new TableRow.LayoutParams(
-//                        TableRow.LayoutParams.WRAP_CONTENT,
-//                        TableRow.LayoutParams.WRAP_CONTENT));
-//                textView.setPadding(16, 16, 16, 16); // Adjust padding as needed
-//                textView.setText("Cell " + i + "-" + j);
-//
-//                // Add the text view to the table row
-//                tableRow.addView(textView);
-//            }
-//
-//            // Add the table row to the table layout
-//            tableLayout.addView(tableRow);
-//        }
+
+        List<String> items = new LinkedList<>();
+        items.add("Tansportation");
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        Goals goal = new Goals(items);
+        recyclerView.setAdapter(goal);
+
+        findViewById(R.id.btnAddExpenses).setOnClickListener(view -> {
+            items.add(0, goal_data[count % 3]);
+            count++;
+            goal.notifyItemInserted(0);
+            recyclerView.scrollToPosition(0);
+        });
     }
 }
