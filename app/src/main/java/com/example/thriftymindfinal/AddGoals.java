afabe624382f6c3b20f343b2811b108488cba2f0
ritchelmendaros <ritchel.mendaros@cit.edu>
2023-05-22 17:31:55 +0800

    Added AddGoals UI

--- app/src/main/java/com/example/thriftymindfinal/AddGoals.java
+++ app/src/main/java/com/example/thriftymindfinal/AddGoals.java
@@ -2,13 +2,26 @@ package com.example.thriftymindfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddGoals extends AppCompatActivity {
-
+    private Button button;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_goals);
+        button = (Button) findViewById(R.id.btnAddGoal);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AddGoals.this, Savings.class);
+                startActivity(intent);
+            }
+        });
     }
 }
