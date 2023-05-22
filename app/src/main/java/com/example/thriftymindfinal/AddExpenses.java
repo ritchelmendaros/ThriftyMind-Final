56ab922daba8966afaae9f694ba34731a467f57e
ritchelmendaros <ritchel.mendaros@cit.edu>
2023-05-22 17:32:21 +0800

    Added AddExpenses

--- app/src/main/java/com/example/thriftymindfinal/AddExpenses.java
+++ app/src/main/java/com/example/thriftymindfinal/AddExpenses.java
@@ -2,13 +2,24 @@ package com.example.thriftymindfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddExpenses extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_expenses);
+        button = (Button) findViewById(R.id.btnAddExpenses);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AddExpenses.this, Dashboard.class);
+                startActivity(intent);
+            }
+        });
     }
 }
